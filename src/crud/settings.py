0450e6f005f3c569399b15b004019ec3aca40254
Cesar Silva <csilvaatc@gmail.com>
2021-02-07 13:04:56 -0300

    pronto para deploy

--- src/crud/settings.py
+++ src/crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = '3vi8&wj*1k7^=2%_nowmu@*304sc-whk*7x7$ubsxjh4&9s)6('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://crud-adi.herokuapp.com/']
 
 
 # Application definition
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
